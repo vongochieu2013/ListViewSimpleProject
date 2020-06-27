08cf38cf257fbc3adf3ecbb031054d89fef9a4fa
vongochieu7111999 <vongochieu2013@gmail.com>
2020-06-27 13:19:37 -0700

    Changing 2 files

--- app/src/main/java/com/example/listviewapplication/DetailActivity.java
+++ app/src/main/java/com/example/listviewapplication/DetailActivity.java
@@ -57,4 +57,6 @@ public class DetailActivity extends AppCompatActivity {
 
 
     }
+
+    // Test DetailActivity
 }
--- app/src/main/java/com/example/listviewapplication/ItemAdapter.java
+++ app/src/main/java/com/example/listviewapplication/ItemAdapter.java
@@ -59,4 +59,6 @@ public class ItemAdapter extends BaseAdapter {
         return v;
     }
 
+    //Test ItemAdapter
+
 }
